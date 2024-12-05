4b92a680cd68fc755c99d7bc10c0f0d0bbb89e92
safeenataj <shama.safeenataj@gmail.com>
2024-12-05 12:37:18 +0100

    Init commit for Meeting Calendar Backend

---
+++ MeetingCalendar_Backend/src/test/java/lexicon/se/meetingcalendar_backend/MeetingCalendarBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package lexicon.se.meetingcalendar_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingCalendarBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
